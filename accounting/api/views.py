23f6d0145461541e85f0084abe79db10746e16a9
adamorujov <ademorujov@gmail.com>
2025-07-19 04:00:21 +0400

    Error fixed in PurchaseCreateAPIView

--- accounting/api/views.py
+++ accounting/api/views.py
@@ -64,7 +64,7 @@ class PurchaseCreateAPIView(CreateAPIView):
             )
 
             response_data = {
-                "message": f"{int(purchase_data["amount"])} Məhsul alındı: {product.name}"
+                "message": f"{int(purchase_data['amount'])} Məhsul alındı: {product.name}"
             }
             return Response(response_data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
