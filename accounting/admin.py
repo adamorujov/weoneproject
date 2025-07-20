078d2f42f26c9e86826701342c498df23e5754cc
adamorujov <ademorujov@gmail.com>
2025-07-20 17:27:36 +0400

    CustomerAction and ProductAction added to admin panel.

--- accounting/admin.py
+++ accounting/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from accounting.models import SaleList
+from accounting.models import SaleList, CustomerAction, ProductAction
 
 admin.site.register(SaleList)
+admin.site.register(CustomerAction)
+admin.site.register(ProductAction)
