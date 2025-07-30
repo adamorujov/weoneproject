099075128b4b009f3242fe92ad2f68ee99a5e927
adamorujov <ademorujov@gmail.com>
2025-07-30 15:59:11 +0400

    PurchaseList added to admin.

--- accounting/admin.py
+++ accounting/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from accounting.models import SaleList, CustomerAction, ProductAction
+from accounting.models import SaleList, PurchaseList, CustomerAction, ProductAction
 
 admin.site.register(SaleList)
+admin.site.register(PurchaseList)
 admin.site.register(CustomerAction)
 admin.site.register(ProductAction)
