38d2e9d376fc3cbf50f7e900a6fa998c231e408e
adamorujov <ademorujov@gmail.com>
2025-08-01 20:36:45 +0400

    SaleListRetrieveSerializer updated.

--- accounting/api/serializers.py
+++ accounting/api/serializers.py
@@ -134,14 +134,16 @@ class SaleListRetrieveSerializer(serializers.ModelSerializer):
 
     def get_old_debt(self, obj):
         customer = obj.salelist_sales.first().customer
-        old_sales = Sale.objects.filter(customer=customer, status="S").exclude(salelist = obj)
+        dt = obj.salelist_sales.first().datetime
+        old_sales = Sale.objects.filter(customer=customer, status="S", datetime__lt=dt).exclude(salelist = obj)
         total_old_price = sum([sale.price * sale.amount for sale in old_sales])
         total_old_debt = total_old_price - self.get_total_paid_amount(obj)
         return total_old_debt if total_old_debt > 0 else 0
 
     def get_new_debt(self, obj):
         customer = obj.salelist_sales.first().customer
-        old_sales = Sale.objects.filter(customer=customer, status="S").exclude(salelist = obj)
+        dt = obj.salelist_sales.first().datetime
+        old_sales = Sale.objects.filter(customer=customer, status="S", datetime__lt=dt).exclude(salelist = obj)
         total_old_price = sum([sale.price * sale.amount for sale in old_sales])
         new_sales = Sale.objects.filter(salelist = obj, status="S")
         total_new_price = sum([sale.price * sale.amount for sale in new_sales])
