a7e190734cd5e2a2dce5fafedde09a915f59b344
adamorujov <ademorujov@gmail.com>
2025-11-27 11:34:03 +0400

    BulkSaleAPIView fixed.

--- accounting/api/views.py
+++ accounting/api/views.py
@@ -763,7 +763,6 @@ class BulkSaleAPIView(APIView):
                 salelist = SaleList.objects.get(id=salelist_id)
                 for i in range(len(products_id)):
                     product = get_object_or_404(Product, id=products_id[i])
-                    dt = timezone.make_aware(datetimes[i])
                     sale, created = Sale.objects.get_or_create(
                         customer = customer,
                         salelist = salelist,
@@ -775,7 +774,7 @@ class BulkSaleAPIView(APIView):
                         sale.seller = request.user
                         sale.status = statuses[i]
                         sale.amount = amounts[i]
-                        sale.datetime = dt
+                        sale.datetime = datetimes[i]
                         sale.price = prices[i]
                         sale.save()
                         if sale.status == "S":
@@ -789,7 +788,7 @@ class BulkSaleAPIView(APIView):
                         old_sale_amount = sale.amount
                         sale.amount = amounts[i]
                         sale.status = statuses[i]
-                        sale.datetime = dt
+                        sale.datetime = datetimes[i]
                         sale.price = prices[i]
                         sale.save()
                         if old_sale_status == "S" and sale.status == "G":
@@ -810,10 +809,11 @@ class BulkSaleAPIView(APIView):
                             if hasattr(product, "stock"):
                                 product.stock.amount = product.amount
                                 product.stock.save()
+
                     ProductAction.objects.create(
                         product = product,
                         customer = customer,
-                        date = dt.date(), 
+                        date = datetimes[i].date(), 
                         sold_product_number = amounts[i],
                         remaining_product_number = product.amount
                     )
@@ -821,21 +821,20 @@ class BulkSaleAPIView(APIView):
                         customeractionlist = customeractionlist,
                         customer = customer,
                         product = product,
-                        date = dt.date(), 
+                        date = datetimes[i].date(), 
                         product_price = prices[i] * amounts[i]
                     )
             else:
                 salelist = SaleList.objects.create()
                 for i in range(len(products_id)):
                     product = get_object_or_404(Product, id=products_id[i])
-                    dt = timezone.make_aware(datetimes[i])
                     sale = Sale.objects.create(
                         seller = seller,
                         customer = customer,
                         salelist = salelist,
                         product = product,
                         amount = amounts[i],
-                        datetime = dt,
+                        datetime = datetimes[i],
                         price = prices[i],
                         status = statuses[i]
                     )
@@ -848,7 +847,7 @@ class BulkSaleAPIView(APIView):
                     ProductAction.objects.create(
                         product = product,
                         customer = customer,
-                        date = dt.date(), 
+                        date = datetimes[i].date(), 
                         sold_product_number = amounts[i],
                         remaining_product_number = product.amount
                     )
@@ -856,7 +855,7 @@ class BulkSaleAPIView(APIView):
                         customeractionlist = customeractionlist,
                         customer = customer,
                         product = product,
-                        date = dt.date(), 
+                        date = datetimes[i].date(), 
                         product_price = prices[i] * amounts[i]
                     )
 
