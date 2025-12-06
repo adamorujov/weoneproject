bd5651de19b610c34c15d9f31cfe9396d5f1db6c
adamorujov <ademorujov@gmail.com>
2025-12-06 20:08:22 +0400

    CustomerActionList added to admin panel.

--- accounting/admin.py
+++ accounting/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from accounting.models import SaleList, PurchaseList, CustomerAction, ProductAction, Stock, Sale
+from accounting.models import SaleList, PurchaseList, CustomerAction, ProductAction, Stock, Sale, CustomerActionList
 
 admin.site.register(SaleList)
 admin.site.register(PurchaseList)
@@ -7,3 +7,4 @@ admin.site.register(CustomerAction)
 admin.site.register(ProductAction)
 admin.site.register(Stock)
 admin.site.register(Sale)
+admin.site.register(CustomerActionList)
